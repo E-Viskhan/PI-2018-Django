8ae07540f1b1ee88171eb6c060a83f99f61f43ed
E-Viskhan <viskhan11@gmail.com>
2021-12-15 14:36:54 +0300

    Добавлены модели

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Contact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE),
+    phone = models.CharField(max_length=30)
+
+    created_at = models.DateTimeField(auto_now_add=True);
--- friends/models.py
+++ friends/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Friend(models.Model):
+    first_contact = models.ForeignKey('contacts.Contact', on_delete=models.CASCADE),
+    second_contact = models.ForeignKey('contacts.Contact', on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('first_contact', 'second_contact')
+
